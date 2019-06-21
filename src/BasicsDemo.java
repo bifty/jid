8f5b9af2270c2be61b49d7f883399f9d7983c285
Sebastian Geiger <bastigeiger@gmail.com>
2019-06-21 23:40:14 +0200

    labeled continue

--- src/BasicsDemo.java
+++ src/BasicsDemo.java
@@ -171,6 +171,22 @@ class BasicsDemo {
         System.out.println("num: " + num); // prints 55
     }
 
+    static void labeledContinue() {
+        System.out.println("\nInside labeledContinue ...");
+        int num = 0;
+
+        outermost: for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i == 5 && j == 5) {
+                    continue outermost;
+                }
+                num++;
+            }
+        }
+
+        System.out.println("num: " + num); // prints 55
+    }
+
     public static void main(String[] args) {
         // Language Basics 1
         //print();
@@ -184,6 +200,7 @@ class BasicsDemo {
         //charTypePromotion();
         //bitwiseOperators();
         //switchExample();
-        labeledBreak();
+        //labeledBreak();
+        labeledContinue();
     }
 }
